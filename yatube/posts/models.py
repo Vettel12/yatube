1e0c168ea25ae701bab00cc83802d5367f8bf9c6
Valery <redbullyagodkin@gmail.com>
2023-05-15 22:16:33 +0300

    New 2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,3 +7,4 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    
